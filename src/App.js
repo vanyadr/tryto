dedbe6798c266c001621d4cee60ade8875d0e0c5
Ivan Drugov <vanyade@yandex.ru>
2022-06-21 14:34:53 +0300

    reInit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div> 
+  );
+}
+
+export default App;
