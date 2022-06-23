f1556429bc645cbb967bb03d9b1cbf0c1f8a44fe
Ivan Drugov <vanyade@yandex.ru>
2022-06-23 13:25:03 +0300

    try to do routs

--- src/components/Header/Menu/Menu.js
+++ src/components/Header/Menu/Menu.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import styles from "./Menu.module.css"
+import { TaskAdder } from "../../../pages"
+
 
 
 export function Menu(){
     return (
-        <div className={ styles.holder }>
-            <div className={ styles.butt}>
-                <a href="/">Board</a>
+        <>
+            <div className={ styles.holder }>
+                <div className={ styles.butt}>
+                    <a href="/">Board</a>
+                </div>
+                <div className={ styles.butt }>
+                    <a href="/add">Add Task</a>
+                </div>
+                <div className={ styles.butt }>
+                    <a href="/contacts">Contacts</a>
+                </div>
             </div>
-            <div className={ styles.butt }>
-                <a href="/about">Add Task</a>
-            </div>
-            <div className={ styles.butt }>
-                <a href="/contacts">Contacts</a>
-            </div>
-        </div>
+
+            <Router>
+                <Routes>
+                    <Route path="/add" element={ TaskAdder } />
+                </Routes>
+            </Router>
+        </>
     );
 }
---
+++ src/pages/TaskAdder/TaskAdder.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { TaskAdd } from '../../components';
+
+export function TaskAdder() {
+  return (
+    <div>
+      Hello
+    </div> 
+  );
+}
+
